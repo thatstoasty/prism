358ff04f9b2bf736f606c749d9ba11c4c58cfeb2
Mikhail Tavarez <miktavarez@gmail.com>
2024-04-23 21:09:19 -0500

    add bash check to tests

--- run_examples.sh
+++ run_examples.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+export MOJO_PYTHON_LIBRARY=$(which python3)
 
-mojo run examples/hello_world/root.mojo say hello
-mojo run examples/nested/nested.mojo get cat --count 5 -l
-mojo run examples/printer/printer.mojo "sample-text" --formatting=underline
-mojo run examples/read_csv/root.mojo --file examples/read_csv/file.csv --lines 1
-mojo run examples/logging/root.mojo --type=json hello
-mojo run examples/persistent_flags_and_cmds/persistent.mojo get cat --count 2 --lover
-mojo run examples/persistent_flags_and_cmds/persistent.mojo get dog --count 2 -l
+mkdir ./temp
+mojo package prism -I ./external -o ./temp/prism.mojopkg
+
+echo -e "Building binaries for all examples...\n"
+mojo build examples/hello_world/root.mojo -o temp/hello_world
+mojo build examples/nested/nested.mojo -o temp/nested
+mojo build examples/printer/printer.mojo -o temp/printer
+mojo build examples/read_csv/root.mojo -o temp/read_csv
+mojo build examples/logging/root.mojo -o temp/logging
+mojo build examples/persistent_flags_and_cmds/persistent.mojo -o temp/persistent
+mkdir -p temp/examples/read_csv/ && cp examples/read_csv/file.csv temp/examples/read_csv/file.csv
+
+echo -e "Executing examples...\n"
+cd temp
+./hello_world say hello
+./nested get cat --count 5 -l
+./printer "sample-text" --formatting=underline
+./read_csv --file examples/read_csv/file.csv --lines 1
+./logging --type=json hello
+./persistent get cat --count 2 --lover
+./persistent get dog -l
+
+cd ..
+rm -R ./temp
